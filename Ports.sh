30d57e29640e09af106d37f53bfebb6de28b93fa
AugustoInnovent <augusto.gabriel@innoventsolutions.com.br>
2025-06-09 13:44:03 -0300

    Update Ports.sh

--- Ports.sh
+++ Ports.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-# Caminho do log do Snort (ajuste se for diferente)
-LOG="/var/log/snort/snort_igc122391/alert"
+# Host configurado no Zabbix (precisa bater com o hostname do item no Zabbix)
+ZABBIX_HOST="PFSENSE-FOR"         
+ZABBIX_SERVER="74.163.81.252"
+ZABBIX_KEY="net.topports.snort"
 
-# Número de portas mais utilizadas a mostrar
-TOP=10
+# Caminho do log Snort
+LOG="/var/log/snort/alert"
 
-# Extraindo portas origem e destino e mostrando as mais frequentes
-echo "Top $TOP portas de ORIGEM:"
-awk -F',' '{print $8}' "$LOG" | sort | uniq -c | sort -nr | head -n $TOP
+# Gerar estatística
+PORT_STATS=$(awk -F',' '{print $8}' "$LOG" | sort | uniq -c | sort -nr | head -n 5)
+PORT_STATS="$PORT_STATS\n"
+PORT_STATS="$PORT_STATS\n$(awk -F',' '{print $10}' "$LOG" | sort | uniq -c | sort -nr | head -n 5)"
 
-echo ""
-echo "Top $TOP portas de DESTINO:"
-awk -F',' '{print $10}' "$LOG" | sort | uniq -c | sort -nr | head -n $TOP
+# Enviar usando zabbix_sender
+echo "$PORT_STATS" | /usr/local/bin/zabbix_sender -z 74.163.81.252 -s "PFSENSE-FOR" -k "net.topports.snort" -T -i -
