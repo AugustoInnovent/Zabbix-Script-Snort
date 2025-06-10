48008676bd2bc67dcd3a50e0ee2a7d0d6bf86000
AugustoInnovent <augusto.gabriel@innoventsolutions.com.br>
2025-06-10 13:50:14 -0300

    Update topip.sh

--- topip.sh
+++ topip.sh
@@ -2,16 +2,16 @@
 
 ZABBIX_HOST="PFSENSE-FOR"
 ZABBIX_SERVER="74.163.81.252"
-ZABBIX_KEY="net.topip.snort"
+ZABBIX_KEY="net.topips.snort"
 LOG="/var/log/snort/snort_igc122391/alert"
 TMP_FILE="/tmp/snort_top_ips.txt"
 
-# Extrai os IPs de origem externos dos alertas (campo 7, IP de origem)
-grep -Eo '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' "$LOG" | \
-    awk 'NR % 2 == 1' | \
+TODAY=$(date "+%m/%d/%y")
+
+grep "^$TODAY" "$LOG" | \
+    awk -F',' '{print $7}' | \
     sort | uniq -c | sort -nr | head -10 > "$TMP_FILE"
 
-# Envia os 10 IPs mais frequentes como um único valor (formato JSON simples)
 JSON="{"
 i=0
 while read -r count ip; do
@@ -21,5 +21,4 @@ while read -r count ip; do
 done < "$TMP_FILE"
 JSON="$JSON}"
 
-# Envia para o Zabbix
 zabbix_sender -z 74.163.81.252 -s "PFSENSE-FOR" -k "$ZABBIX_KEY" -o "$JSON"
