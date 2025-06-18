809c98590bde01a5dbee35caecc07c9c79f40f63
AugustoInnovent <augusto.gabriel@innoventsolutions.com.br>
2025-06-18 09:26:27 -0300

    Update topprotopf.sh

--- topprotopf.sh
+++ topprotopf.sh
@@ -3,16 +3,29 @@
 LOG="/var/log/filterlog.log"
 
 if [ ! -f "$LOG" ]; then
-    echo "Arquivo de log não encontrado: $LOG"
+    echo "Log não encontrado: $LOG"
     exit 1
 fi
 
-awk -F',' '
+DATES=$(for i in 0 1 2 3; do date -d "-$i days" "+%Y-%m-%d"; done)
+
+awk -v dates="$DATES" '
+BEGIN {
+    split(dates, daylist)
+    for (i in daylist) daymap[daylist[i]] = 1
+    FS = ","
+}
 {
-    for (i = 1; i <= NF; i++) {
-        if ($i ~ /^(UDP|TCP|ICMP|icmp|udp|tcp)$/) {
-            proto[toupper($i)]++
-            break
+    match($0, /^[^ ]+ +[^ ]+ +[^T]+T[0-9:.+-]+/, ts)
+    split(ts[0], datepart, "T")
+    logdate = datepart[1]
+
+    if (logdate in daymap) {
+        for (i = 1; i <= NF; i++) {
+            if ($i ~ /^(UDP|TCP|ICMP|icmp|udp|tcp)$/) {
+                proto[toupper($i)]++
+                break
+            }
         }
     }
 }
