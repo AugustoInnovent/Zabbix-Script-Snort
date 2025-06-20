1f2a6f20b34defdaa8ca346f6ae7ea06b433673c
AugustoInnovent <augusto.gabriel@innoventsolutions.com.br>
2025-06-20 10:56:15 -0300

    Update topprotopf.sh

--- topprotopf.sh
+++ topprotopf.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
-LOG_FILE="/var/log/filter.log"  
+ZABBIX_HOST="PFSENSE-FOR"
+ZABBIX_SERVER="74.163.81.252"
+ZABBIX_KEY="net.topprotos.pf"
+LOG_FILE="/var/log/filter.log"
+TMP_FILE="/tmp/top_protos.txt"
+
+
 LIMITE_EPOCH=$(date -j -v-3d "+%s")
 
 awk -v limite="$LIMITE_EPOCH" '
-function get_epoch(date_str,   cmd, epoch) {
-    gsub(/\..*/, "", date_str)  # Remove microsegundos e timezone
-    cmd = "date -j -f \"%Y-%m-%dT%H:%M:%S\" \"" date_str "\" +%s"
+function get_epoch(date_str,   clean, cmd, epoch) {
+    gsub("T", "", date_str)           # Junta data e hora
+    gsub("[-:]", "", date_str)        # Remove separadores
+    gsub(/\..*/, "", date_str)        # Remove microsegundos e timezone
+    clean = substr(date_str, 1, 12)   # YYYYMMDDHHMM
+    clean = clean ".00"               # Adiciona segundos
+    cmd = "date -j -f \"%Y%m%d%H%M.%S\" \"" clean "\" +%s"
     cmd | getline epoch
     close(cmd)
     return epoch
@@ -25,8 +35,20 @@ function get_epoch(date_str,   cmd, epoch) {
     }
 }
 END {
-    print "Ranking de protocolos nos últimos 3 dias:\n"
     for (p in counts) {
-        printf "%s: %d\n", p, counts[p]
+        printf "%d %s\n", counts[p], p
     }
-}' "$LOG_FILE" | sort -k2 -nr
+}' "$LOG_FILE" | sort -nr | head -10 > "$TMP_FILE"
+
+
+JSON="{"
+i=0
+while read -r count proto; do
+    [ $i -gt 0 ] && JSON="$JSON,"
+    JSON="$JSON\"$proto\":$count"
+    i=$((i+1))
+done < "$TMP_FILE"
+JSON="$JSON}"
+
+
+zabbix_sender -z 74.163.81.252 -s "PFSENSE-FOR" -k "$ZABBIX_KEY" -o "$JSON"
