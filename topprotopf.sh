37dc523a93238434fbb9ba95b9bcf88fbc2aa41d
AugustoInnovent <augusto.gabriel@innoventsolutions.com.br>
2025-06-20 10:47:27 -0300

    Update topprotopf.sh

--- topprotopf.sh
+++ topprotopf.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 
 LOG_FILE="/var/log/filter.log"  
-
 LIMITE_EPOCH=$(date -j -v-3d "+%s")
 
 awk -v limite="$LIMITE_EPOCH" '
 function get_epoch(date_str,   cmd, epoch) {
-    gsub("T.*", "", date_str)  # Remove hora
-    cmd = "date -j -f \"%Y-%m-%d\" \"" date_str "\" +%s"
+    gsub(/\..*/, "", date_str)  # Remove microsegundos e timezone
+    cmd = "date -j -f \"%Y-%m-%dT%H:%M:%S\" \"" date_str "\" +%s"
     cmd | getline epoch
     close(cmd)
     return epoch
 }
 {
-    data_iso = $2  # Ex: 2025-06-18T09:07:00.123456-03:00
+    data_iso = $2  # Ex: 2025-06-18T09:07:00.458077-03:00
     epoch = get_epoch(data_iso)
     if (epoch >= limite) {
         for (i=1; i<=NF; i++) {
