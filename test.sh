c6d538a6e05b332bd8387349b1e2b3e1f5a05ad6
AugustoInnovent <augusto.gabriel@innoventsolutions.com.br>
2025-07-31 14:16:03 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,9 +3,9 @@
 LOG_FILE="/var/log/snort/snort_igc122391/alert"
 TODAY=$(date +"%m/%d/%y")
 
-TCP=$(grep "^$TODAY" "$LOG_FILE" | grep ",TCP," | wc -l)
-UDP=$(grep "^$TODAY" "$LOG_FILE" | grep ",UDP," | wc -l)
-ICMP=$(grep "^$TODAY" "$LOG_FILE" | grep ",ICMP," | wc -l)
+TCP=$(grep "^$TODAY" /var/log/snort/snort_igc122391/alert | grep ",TCP," | wc -l)
+UDP=$(grep "^$TODAY" /var/log/snort/snort_igc122391/alert | grep ",UDP," | wc -l)
+ICMP=$(grep "^$TODAY" /var/log/snort/snort_igc122391/alert | grep ",ICMP," | wc -l)
 
 echo "$TCP"
 echo "$UDP"
