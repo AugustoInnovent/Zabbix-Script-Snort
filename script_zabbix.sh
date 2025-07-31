b7845a9ca08989912b2696a9429bd91fddaa5345
AugustoInnovent <augusto.gabriel@innoventsolutions.com.br>
2025-07-31 09:30:20 -0300

    Update script_zabbix.sh

--- script_zabbix.sh
+++ script_zabbix.sh
@@ -4,13 +4,11 @@ LOG_FILE="/var/log/snort/snort_igc122391/alert"
 
 TODAY=$(date +"%m/%d/%y")
 
-ZBX_SERVER="74.163.81.252"
-ZBX_HOST="PFSENSE-FOR"
 
 TCP=$(grep "^$TODAY" "$LOG_FILE" | grep ",TCP," | wc -l)
 UDP=$(grep "^$TODAY" "$LOG_FILE" | grep ",UDP," | wc -l)
 ICMP=$(grep "^$TODAY" "$LOG_FILE" | grep ",ICMP," | wc -l)
 
-/usr/local/bin/zabbix_sender -z "74.163.81.252" -s "PFSENSE-FOR" -k snort.proto.tcp.daily -o "$TCP"
-/usr/local/bin/zabbix_sender -z "74.163.81.252" -s "PFSENSE-FORT" -k snort.proto.udp.daily -o "$UDP"
-/usr/local/bin/zabbix_sender -z "74.163.81.252" -s "PFSENSE-FOR" -k snort.proto.icmp.daily -o "$ICMP"
+/usr/local/bin/zabbix_sender -z 74.163.81.252 -s "PFSENSE-FOR" -k snort.proto.tcp.daily -o "$TCP"
+/usr/local/bin/zabbix_sender -z 74.163.81.252 -s "PFSENSE-FORT" -k snort.proto.udp.daily -o "$UDP"
+/usr/local/bin/zabbix_sender -z 74.163.81.252 -s "PFSENSE-FOR" -k snort.proto.icmp.daily -o "$ICMP"
