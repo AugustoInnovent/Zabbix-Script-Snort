a3f6f0031027bf0424b74866fcfd070eccc5ee13
AugustoInnovent <augusto.gabriel@innoventsolutions.com.br>
2025-08-04 10:21:28 -0300

    Update protoSP.sh

--- protoSP.sh
+++ protoSP.sh
@@ -5,8 +5,8 @@ LOG_FILE="/var/log/snort/snort_igc231737/alert"
 TODAY=$(date +"%m/%d/%y")
 
 TCP=$(grep "^$TODAY" ../../../../var/log/snort/snort_igc231737/alert | grep ",TCP," | wc -l)
-/usr/local/bin/zabbix_sender -z 74.163.81.252 -s "PFSENSE-FOR" -k snort.proto.tcp.daily -o "$TCP"
+/usr/local/bin/zabbix_sender -z 74.163.81.252 -s "PFSENSE-SP" -k snort.proto.tcp.daily -o "$TCP"
 UDP=$(grep "^$TODAY" ../../../../var/log/snort/snort_igc231737/alert | grep ",UDP," | wc -l)
-/usr/local/bin/zabbix_sender -z 74.163.81.252 -s "PFSENSE-FOR" -k snort.proto.udp.daily -o "$UDP"
+/usr/local/bin/zabbix_sender -z 74.163.81.252 -s "PFSENSE-SP" -k snort.proto.udp.daily -o "$UDP"
 ICMP=$(grep "^$TODAY" ../../../../var/log/snort/snort_igc231737/alert | grep ",ICMP," | wc -l)
-/usr/local/bin/zabbix_sender -z 74.163.81.252 -s "PFSENSE-FOR" -k snort.proto.icmp.daily -o "$ICMP"
+/usr/local/bin/zabbix_sender -z 74.163.81.252 -s "PFSENSE-SP" -k snort.proto.icmp.daily -o "$ICMP"
